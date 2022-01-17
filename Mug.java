90066ce868429448118d111748af7ebb72bfac4d
brianzib <brianzib2005@gmail.com>
2022-01-17 22:22:21 +0000

    Added Mug OOP program

---
+++ Mug.java
@@ -0,0 +1,22 @@
+public class Mug
+{
+
+
+private int iCapacity;
+private int iFilled;
+private int iValue;
+private double dTemperature;
+
+private boolean bLiquid = false;
+private String szMaterial;
+private String szColour;
+
+private static void getCapacity()
+{
+    iCapacity = iValue;
+
+    
+}
+
+
+}
