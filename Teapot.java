42735174bbeaea32af93ce812f63344bfaa069c1
brianzib <brianzib2005@gmail.com>
2022-01-27 17:59:14 +0000

    Update Teapot.java

--- Teapot.java
+++ Teapot.java
@@ -2,9 +2,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+enum Material {
+
+	STEEL, CLAY, TIN;
+
+}
+
 public class Teapot {
 
-	private String szMaterial;
+	private Material szMaterial;
 	private String szColour;
 
 	private int iCapacity;
@@ -23,7 +29,6 @@ public class Teapot {
 	public void reset()
 	{
 		setCapacity(300);
-		setMaterial("Clay");
 		setTemperature(25);
 		setFill(0);
 		setColour("Blue");
@@ -34,7 +39,6 @@ public class Teapot {
 	public void wash()
 	{
 		setCapacity(0);
-		setMaterial("");
 		setTemperature(0);
 		setFill(0);
 		setColour("");
@@ -49,7 +53,7 @@ public class Teapot {
 			return;
 		}
 	}
-	
+
 	private void setBoiled(int i) 
 	{
 		if (i < 80)
@@ -57,7 +61,7 @@ public class Teapot {
 			bBoiled = false;
 			return;
 		}
-		
+
 		else 
 		{
 			bBoiled = true;
@@ -70,9 +74,9 @@ public class Teapot {
 		return;
 	}
 
-	public void setMaterial(String szElement)
+	public void setMaterial(Material m)
 	{
-		szMaterial = szElement;
+		szMaterial = m;
 		return;
 	}
 
@@ -102,15 +106,6 @@ public class Teapot {
 		}
 
 		return;
-	}
-	
-	public void setContainer()
-	{
-		
-		ContainerColour();
-		
-		return;
-		
 	}
 
 	private String getColour() 
@@ -148,7 +143,7 @@ public class Teapot {
 		return(iCapacity);
 	}
 
-	public String getMaterial()
+	public Material getMaterial()
 	{
 		return(szMaterial);
 	}
@@ -165,58 +160,76 @@ public class Teapot {
 		{
 			System.out.println("The Teapot has been boiled, and is currently at the temperature of: " + getTemperature());
 		}
-		
+
 		else 
 		{
-		System.out.println("The teapot has not been boiled and currently has the temperature of: " + getTemperature());	
+			System.out.println("The teapot has not been boiled and currently has the temperature of: " + getTemperature());	
 		}
-		
+
 		if (bEmpty = false)
 		{
-		System.out.println("The current holding volume of the teapot is: " + getVolume());
+			System.out.println("The current holding volume of the teapot is: " + getVolume());
 		}
-		
+
 		else 
 		{
 			System.out.println("The teapot currently has no liquid inside");
 		}
 	}
-	
+
 	public void set() throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
+
+
 		System.out.println("Please enter the capacity of the teapot ");
 		String szInput = br.readLine();
 		int i = Integer.parseInt(szInput);
-		
+
 		setCapacity(i);
-		
+
 		System.out.println("Please enter the colour of the teapot ");
 		szInput = br.readLine();
-		
+
 		setColour(szInput);
-		
-		System.out.println("Please enter the material of the teapot ");
+
+		System.out.println("Please enter the material of the teapot, steel, clay or tin. ");
 		szInput = br.readLine();
+
+		szInput = szInput.toLowerCase();
+
+		if (szInput == "steel")
+		{
+			Material m = Material.STEEL;
+			setMaterial(m);
+		}
 		
-		setMaterial(szInput);
+		if (szInput == "clay")
+		{
+			Material m = Material.CLAY;
+			setMaterial(m);
+		}
 		
+		if (szInput == "tin")
+		{
+			Material m = Material.TIN;
+			setMaterial(m);
+		}
+
 		System.out.println("Please enter the current volume of the teapot ");
 		szInput = br.readLine();
 		i = Integer.parseInt(szInput);
-		
+
 		setVolume(i);
 		setFill(i);
-		
+
 		System.out.println("Please enter the current temperature of the teapot ");
 		szInput = br.readLine();
 		i = Integer.parseInt(szInput);
-		
+
 		setTemperature(i);
 		setBoiled(i);
-			
+
 	}
 
 
@@ -226,15 +239,17 @@ public class Teapot {
 	public static void main(String[] args) throws IOException {
 
 		Teapot myTeapot = new Teapot();
-		
+
 		myTeapot.set();
-		
+
 		myTeapot.Display();
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
 
 	}
 
